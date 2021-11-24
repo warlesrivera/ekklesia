69fd5fab42a6721d6e3f3e37201a0859dafdb892
warles123 <warlesrivera123@gmail.com>
2021-11-24 16:47:17 -0500

    estilos

---
+++ public/assets/js/style.js
@@ -0,0 +1,26 @@
+
+  function openMenu(){
+    if($("#menu").hasClass("menu-background-grey")){
+            $("#menu").removeClass("menu-background-grey");
+            $("#menu").addClass("color-transparent");
+            $("#items-menu").removeClass("menu-background-solid");
+            $("#menu-bars,#items-menu >li >a").removeClass("text-dark");
+            $("#menu-bars,#items-menu >li >a").addClass("text-white");
+            $("#items-menu >li").removeClass("li-black");
+    }else{
+            $("#menu").addClass("menu-background-grey");
+            $("#menu").removeClass("color-transparent");
+            $("#menu-bars,#items-menu >li >a").addClass("text-dark");
+            $("#items-menu >li >a").css("font-weight","500");
+            $("#menu-bars,#items-menu >li >a").removeClass("text-white");
+            $("#items-menu >li").addClass("li-black");
+
+
+
+
+
+    }
+
+
+  }
+
