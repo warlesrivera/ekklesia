9e624719e1d068145a852da6988d469d50053af1
warles123 <warlesrivera123@gmail.com>
2021-11-24 21:40:23 -0500

    ajustes responsives

--- resources/views/components/nosotros.blade.php
+++ resources/views/components/nosotros.blade.php
@@ -1,12 +1,23 @@
 <div class="col-12 p-0 m-0 item-black row">
     <div class="row justify-content-center col-md-5 col-12 mr-md-0 ml-md-5 mt-md-5 mx-0  px-0 pl-md-5 pr-md-0 pt-2 pt-md-2">
-        <div class="col-md-12 p-0 m-0 col-10">
-            <img width="100%" src="{{asset('assets/images/home-persona.png')}}" alt="">
+        <div class="col-md-12 p-0 m-0 pb-5 col-10">
+            <img  width="100%" height="100%" src="{{asset('assets/images/home-persona.png')}}" alt="">
         </div>
     </div>
-    <div class="row justify-content-center col-12 col-md-6 px-0 pt-md-0 pt-4 mx-0 mt-md-5   ">
-        <div class="col-md-12 col-10 row justify-content-center p-0 m-0">
-            <img width="80%" src="{{asset('assets/images/olas.png')}}" alt="">
+    <div class="col-12 col-md-5 pl-md-5 ml-md-5 mt-2 pt-md-5 px-md-0 ">
+        <div class="col-md-8  col-10 row mx-auto mx-md-0 pt-3  justify-content-md-start pl-0 p-0 m-0">
+            <img height="100%" width="100%" src="{{asset('assets/images/olas.png')}}" alt="">
+        </div>
+        <div class="col-md-8 col-12  pt-4 mt-md-5  ">
+            <img height="100%" width="100%" src="{{asset('assets/images/un-lugar-para-todos.png')}}" alt="">
+        </div>
+        <div class="col-12 col-md-8  mt-2 pt-md-5 px-md-0">
+            <p class=" text-start text-white titulo-light size-23" >
+                Creemos en una iglesia que ama a todas las personas, que no discrimina ni tiene favoritismos, que no juzga ni señala el pecado de nadie y que honra y dignifica a cada persona por igual.
+            </p>
+        </div>
+        <div class="col-md-5 col-9 row justify-content-end pt-5  border-bottom mb-4 ">
+            <a href="" class="titulo size-23" >CONÓCENOS ></a>
         </div>
     </div>
 </div>
--- resources/views/components/nuestraciudad.blade.php
+++ resources/views/components/nuestraciudad.blade.php
@@ -1,13 +1,16 @@
 
 <div id="nuestraciudad" class="col-12 p-0 m-0 row justify-content-center ">
     <div class="w-100 py-md-4 my-md-5"></div>
-    <div class="col-md-10 col-12 mx-auto text-center row justify-content-center py-4">
-        <h1 class="titulo-gr py-0 my-0 ">Nuestra Ciudad</h1>
-        <p class="col-md-6 px-md-3 text-white text-justify">
+    <div class="col-md-10 col-12 mx-auto pt-md-5 mt-md-5 text-center row justify-content-center py-4">
+        <h1 class="titulo-gr  size-141 py-0 my-0 d-none d-md-block col-12">Nuestra Ciudad</h1>
+        <h1 class="titulo-gr px-0 pt-5 size-77 py-0 my-0 d-block d-md-none col-12">Nuestra Ciudad</h1>
+        <p class="col-md-7 px-md-3 text-white text-center" style="font-size: 26px">
                 BOGOTÁ - 2.600 metros más cerca de las estrellas y nuestra misión, ser un puente que conecte el cielo con nuestra ciudad.
         </p>
     </div>
-    <div class="col-md-10 col-12 row justify-content-center py-md-5 my-md-5">
+    <div class="w-100 py-md-4 my-md-5"></div>
+
+    <div class="col-md-10 col-12 pb-5 pb-md-0 row justify-content-center py-md-5 my-md-5">
         <a  class="btn col-md-3 col-12 mt-1 mt-md-0 px-0 mx-1 btn-outline-light titulo-light">INSCRÍBETE EXPERIENCIA </a>
         <a  class="btn col-md-3 col-12 mt-1 mt-md-0 px-0 mx-1 btn-light titulo-bold">EKKLESIA ONLINE</a>
     </div>
