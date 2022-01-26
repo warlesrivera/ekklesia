f88351ace0b493706c63cd4d0fb445b6619eee37
warlesrivera <warlesrivera123@gmail.com>
2022-01-26 17:51:25 -0500

    egroup

--- app/Modulos/Egroup/Repositorio/EgroupRepositorio.php
+++ app/Modulos/Egroup/Repositorio/EgroupRepositorio.php
@@ -1,12 +1,32 @@
 <?php
 
-namespace App\Modulos\Blog\Repositorio;
+namespace App\Modulos\Egroup\Repositorio;
 
 use App\Models\Egroup;
 
 class EgroupRepositorio
 {
-    public function list(){}
+    public function list(){
+        $blog = Egroup::all();
+
+        if(Auth::user()->roles()->first()->id!=3)
+            $blog = Egroup::where('state', Config::get('constants.ACTIVO'))->orWhere('user_id',Auth::id())->get();
+
+        return datatables()->of($blog)
+            ->addColumn('action', function ($row) {
+
+                $route=Str::of($row->title)->slug('-');
+                $row->url=route('blog.show', "{$row->id}-{$route}");
+                $row->url_destroy=route('egroup.destroy', [$row->id]);
+                $type='Egroup';
+                return view()->make('admin.components.button_action',compact('row','type'));
+            })
+            ->addColumn('users', function ($row) {
+                return $row->users->count();
+            })
+            ->toJson();
+
+    }
     public function store( $request){}
     public function show(Egroup $egroup){}
     public function update( $request, Egroup $egroup){}
